2f57aaa420b1c705322809d25cc4c447868c7bd5
ancamariac <ciuciuancamaria@gmail.com>
2020-09-22 16:26:35 +0300

    Logo code

--- main.c
+++ main.c
@@ -8,12 +8,20 @@ int main(void)
     const int screenHeight = 1000;
 
     InitWindow(screenWidth, screenHeight, "Space Raid");
-    
+
+    /* InitAudioDevice();      // Initialize audio device */
+
     Texture2D background = LoadTexture("assets/background.png");
+
+    Texture2D logo = LoadTexture("assets/logov2.png");
+
+    //Texture2D tap_to_start = LoadTexture("assets/taptostart.png");
+
+    /* Sound fxButton = LoadSound("resources/buttonfx.wav");   // Load button sound */
 
     float scrollingBack = 0.0f;
 
-    SetTargetFPS(60);               
+    SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
 
     // Main game loop
@@ -34,9 +42,11 @@ int main(void)
 
             // Draw background image twice
             // NOTE: Texture is scaled twice its size
-            DrawTextureEx(background, (Vector2){ scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
-            DrawTextureEx(background, (Vector2){ background.width*2 + scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
+            DrawTextureEx(background, (Vector2){ scrollingBack, 0 }, 0.0f, 2.0f, WHITE);
+            DrawTextureEx(background, (Vector2){ background.width*2 + scrollingBack, 0 }, 0.0f, 2.0f, WHITE);
 
+            // Draw logo image
+            DrawTexture(logo, screenWidth/2 - logo.width/2, screenHeight/2 - logo.height/2 - 110, WHITE);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -45,6 +55,8 @@ int main(void)
     // De-Initialization
     //--------------------------------------------------------------------------------------
     UnloadTexture(background);  // Unload background texture
+    UnloadTexture(background);  // Unload logo texture
+    /* UnloadSound(fxButton);      // Unload button sound */
 
     CloseWindow();              // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
